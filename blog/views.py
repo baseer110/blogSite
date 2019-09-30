4d2f19290603decb71f729c765a545820993a869
Baseer Jafri <bjafri@springboardmgmt.com>
2019-09-29 23:42:20 -0400

    Create Post detail page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, postid):
+    post = get_object_or_404(Post, pk=postid)
+    print(postid)
+    print(post)
+    print(post.title)
+    return render(request, 'blog/post_detail.html', {'post': post})
